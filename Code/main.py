98b50a7cf80f7dad41c09359fbeaf68527f3470f
Joel Delgado <joel.delgado@epn.edu.ec>
2023-08-22 00:06:58 -0500

    Configuración de pantalla de inicio y pausa

--- Code/main.py
+++ Code/main.py
@@ -12,44 +12,46 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
     game = Game(screen, screen_width, screen_height)
+    main_menu = MainMenu(screen_width, screen_height)
 
     ALIENLASER = pygame.USEREVENT + 1
     pygame.time.set_timer(ALIENLASER,800)
     in_main_menu = True
     
     while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if in_main_menu:
-            action = main_menu.handle_event(event)
-            if action == "start_game":
-                in_main_menu = False
-            elif action == "quit":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        else:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_p:
-                    game.paused = not game.paused 
-            if not game.paused:
-                
-                if event.type == ALIENLASER:
-                   game.alien_shoot()
-    screen.fill((30, 30, 30))
+            if in_main_menu:
+                action = main_menu.handle_event(event)
+                if action == "start_game":
+                    in_main_menu = False
+                elif action == "quit":
+                    pygame.quit()
+                    sys.exit()
+            else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_p:
+                        game.paused = not game.paused
+                if not game.paused:
+                    if event.type == ALIENLASER:
+                        game.alien_shoot()
 
-    if in_main_menu:
-        main_menu.draw(screen)
-    else:
-        if game.paused:
-           pause_message = game.font.render("PAUSA", True, (255, 255, 255))
-           screen.blit(pause_message, (screen_width // 2 - pause_message.get_width() // 2, screen_height // 2 - pause_message.get_height() // 2))
-        else:
-          game.run()
+        screen.fill((30, 30, 30))
 
-    pygame.display.flip()
-    clock.tick(60)
+        if in_main_menu:
+            main_menu.draw(screen)
+        else:
+            if game.paused:
+                pause_message = game.font.render("PAUSA", True, (255, 255, 255))
+                screen.blit(pause_message, (screen_width // 2 - pause_message.get_width() // 2,
+                                            screen_height // 2 - pause_message.get_height() // 2))
+            else:
+                game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
 
 
 
